214645f9e40dfe01f818e23bfb5e378b3ffd3f69
Magdalena Wilk <ma.wilk@tlen.pl>
2016-06-07 14:57:47 +0200

    Test message send/receive time

--- src/auctions/Auction.java
+++ src/auctions/Auction.java
@@ -2,21 +2,35 @@ package auctions;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
 public class Auction extends Agent {
 
 	protected void setup() {
-//		System.out.println(getLocalName() + " is up");
-		ACLMessage msg = new ACLMessage(ACLMessage.CONFIRM);
-		msg.addReceiver(new AID("tester", AID.ISLOCALNAME));
-		send(msg);
-		try {
-			Thread.sleep(30);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		doDelete();
+		addBehaviour(new CyclicBehaviour(this) {
+
+			@Override
+			public void action() {
+				ACLMessage msg = receive();
+
+				if (msg != null) {
+					if (msg.getPerformative() == ACLMessage.REQUEST) {
+						ACLMessage response = new ACLMessage(ACLMessage.CONFIRM);
+						response.addReceiver(new AID("tester", AID.ISLOCALNAME));
+						send(response);
+						try {
+							Thread.sleep(10);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						doDelete();
+					}
+				} else {
+					block();
+				}
+			}
+		});
 	}
 }
--- src/auctions/Tester.java
+++ src/auctions/Tester.java
@@ -1,5 +1,9 @@
 package auctions;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
@@ -12,16 +16,24 @@ import jade.wrapper.PlatformController;
 
 public class Tester extends Agent {
 
+	private static final String DELIMITER = ";";
+	private static final String NEW_LINE = "\n";
+	private FileWriter fileWriter;
+	
 	private final int N = 1000;
 	private final int incr = 1000;
-	private final int maxN = 80000;
+	private final int maxN = 50000;
 	private int count = 0;
 	private int currentN = N; 
+	private int waitToSetup = 300;
 
 	private long start, end;
 
 	protected void setup() {
+		
 		try {
+			fileWriter = new FileWriter("test2.txt");
+			
 			System.out.println(getLocalName() + " is up");
 			DFAgentDescription dfd = new DFAgentDescription();
 			dfd.setName(getAID());
@@ -40,12 +52,31 @@ public class Tester extends Agent {
 							if (count == currentN) {
 								end = System.nanoTime();
 								count = 0;
+								
+								try {
+									fileWriter.append(String.valueOf(currentN));
+									fileWriter.append(DELIMITER);
+									fileWriter.append(String.valueOf((double) (end - start) / 1000000000.0));
+									fileWriter.append(NEW_LINE);
+								} catch (IOException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+								
 								System.out.println(currentN + " " + (double) (end - start) / 1000000000.0);
 								if(currentN == maxN) {
 									terminate();
 								} else {
 									currentN += incr;
 									createAuctions(currentN);
+									try {
+										Thread.sleep(waitToSetup);
+									} catch (InterruptedException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									start = System.nanoTime();
+									sendRequests(currentN);
 								}
 							}
 						}
@@ -56,9 +87,15 @@ public class Tester extends Agent {
 				}
 			});
 
-			createAuctions(N);
+			createAuctions(currentN);
+			Thread.sleep(waitToSetup);
+			start = System.nanoTime();
+			sendRequests(currentN);
 
 		} catch (FIPAException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -67,11 +104,9 @@ public class Tester extends Agent {
 	private void createAuctions(int n) {
 		try {
 			PlatformController container = getContainerController();
-			start = System.nanoTime();
 			for (int i = 0; i < n; i++) {
 				String name = "a" + (i + currentN);
 				AgentController auction;
-
 				auction = container.createNewAgent(name, "auctions.Auction", null);
 				auction.start();
 			}
@@ -82,11 +117,23 @@ public class Tester extends Agent {
 
 	}
 
+	private void sendRequests(int n) {
+		start = System.nanoTime();
+		for (int i = 0; i < n; i++) {
+			String name = "a" + (i + currentN);
+			ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+			msg.addReceiver(new AID(name, AID.ISLOCALNAME));
+			send(msg);
+		}
+	}
+	
 	protected void terminate() {
-		try {
+        try {
+        	fileWriter.flush();
+			fileWriter.close();
 			DFService.deregister(this);
 			doDelete();
-		} catch (FIPAException e) {
+		} catch (FIPAException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
